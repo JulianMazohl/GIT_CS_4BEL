dce6bd00ffe6f06432645b09def2b7f545a360cb
02mazjul@rgtfo-me.it <62942784+JulianMazohl@users.noreply.github.com>
2020-04-17 09:10:53 +0200

    TExtfarbe einstellbar

--- Zahlensysteme/Zahlensysteme/Program.cs
+++ Zahlensysteme/Zahlensysteme/Program.cs
@@ -10,16 +10,18 @@ namespace Zahlensysteme
         static void Main(string[] args)
         {
 
-            Titel("Das ist ein Test");
-
+            Titel("Das ist ein Test", ConsoleColor.Red);
+            Console.WriteLine("Das ist das Programm");
             Console.ReadKey();
         }
 
-        static void Titel(string text)
+        static void Titel(string text, ConsoleColor textfarbe)
         {
+            ConsoleColor origTextFarbe = Console.ForegroundColor;
             int breite = Console.WindowWidth;
             int textlen = text.Length;
             Console.Clear();
+            Console.ForegroundColor = textfarbe;
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("-");
@@ -34,6 +36,8 @@ namespace Zahlensysteme
             {
                 Console.Write("-");
             }
+            Console.WriteLine();
+            Console.ForegroundColor = origTextFarbe;
         }
     }
 }
