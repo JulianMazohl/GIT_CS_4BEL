9fd05d4a509a2a30eb4cdba1552def5d18bb055b
02mazjul@rgtfo-me.it <62942784+JulianMazohl@users.noreply.github.com>
2020-04-17 09:03:04 +0200

    Titel zentriert

--- Zahlensysteme/Zahlensysteme/Program.cs
+++ Zahlensysteme/Zahlensysteme/Program.cs
@@ -17,9 +17,17 @@ namespace Zahlensysteme
 
         static void Titel(string text)
         {
+            int breite = Console.WindowWidth;
+            int textlen = text.Length;
+            Console.Clear();
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("-");
+            }
+            
+            for (int i = 0; i < (breite-textlen)/2; i++)
+            {
+                Console.Write(" ");
             }
             Console.WriteLine(text);
             for (int i = 0; i < Console.WindowWidth; i++)
