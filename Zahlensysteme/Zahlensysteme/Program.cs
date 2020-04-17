466065f4e63a367620262fa3ef0f333cddbd81e5
02mazjul@rgtfo-me.it <62942784+JulianMazohl@users.noreply.github.com>
2020-04-17 08:58:58 +0200

    TItelMethode erzeugt

--- Zahlensysteme/Zahlensysteme/Program.cs
+++ Zahlensysteme/Zahlensysteme/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+//**Programm zum umwandeln in verschiedene Zahlensysteme
 namespace Zahlensysteme
 {
     class Program
@@ -10,9 +10,22 @@ namespace Zahlensysteme
         static void Main(string[] args)
         {
 
-
+            Titel("Das ist ein Test");
 
             Console.ReadKey();
+        }
+
+        static void Titel(string text)
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine(text);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
         }
     }
 }
